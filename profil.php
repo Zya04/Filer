6168309e689581e5ba8e34bbfc595a564b727241
Zya Nassurally <zya1999@hotmail.fr>
2018-01-12 00:51:52 +0100

    translate french words in english

--- profil.php
+++ profil.php
@@ -1,13 +1,27 @@
 <?php
 require ('init.php');
 
+if(!empty($_FILES)){ 
+    $file_name = $_FILES['file']['name'];
+    $file_extension = strrchr($file_name, ".");
+}
+
 $title = "profil";
 ?>
 <div class="col_profil">
-    <h2>Bienvenue sur ton profil </h2>
+    <h2>Welcome in your profile! </h2>
 </div>
-<input type="file" name="file">ajoute ton fichier.
-<input type="submit" name="sendFile">ajouter
+<input type="file" name="file"> add your file here! <br>
+<input type="submit" name="sendFile" value="add"> <br> <br>
+<table class="table_profile" align="center">
+    <tr>
+        <td>file</td>
+        <td>edit</td>
+        <td>delete</td>
+        <td>download</td>
+    </tr>
+
+</table>
 
 <?php
 $content = ob_get_contents();
