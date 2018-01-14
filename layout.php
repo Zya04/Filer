bd43bc9327d92909478dcfe6b2a47d339357b3c4
Zya Nassurally <zya1999@hotmail.fr>
2018-01-14 17:43:06 +0100

    i don't know what happen but i need to commit

--- layout.php
+++ layout.php
@@ -4,7 +4,6 @@
     <title><?php echo $title; ?></title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
     <link rel="stylesheet" href="style.css">
   </head>
   <body>
