9a000854f3a19357eb086058fa814abefc514881
Zya Nassurally <zya1999@hotmail.fr>
2018-01-14 17:45:10 +0100

    can now upload file

--- profil.php
+++ profil.php
@@ -1,27 +1,43 @@
 <?php
 require ('init.php');
-
-if(!empty($_FILES)){ 
-    $file_name = $_FILES['file']['name'];
-    $file_extension = strrchr($file_name, ".");
+if(isset($_POST['sendFile'])){ 
+    $file_name = $_FILES['files']['name'];
+    $file_tmp_name = $_FILES ['files']['tmp_name'];
+    $file_dest = 'users/'.$file_name;
+    $success = $file_name;
+    if (move_uploaded_file($file_tmp_name, $file_dest)) {
+        $success;
+    } else {
+        echo "sa marche po";
+    }
 }
 
 $title = "profil";
 ?>
-<div class="col_profil">
-    <h2>Welcome in your profile! </h2>
-</div>
-<input type="file" name="file"> add your file here! <br>
-<input type="submit" name="sendFile" value="add"> <br> <br>
-<table class="table_profile" align="center">
-    <tr>
-        <td>file</td>
-        <td>edit</td>
-        <td>delete</td>
-        <td>download</td>
-    </tr>
-
-</table>
+<article>
+    <div class="col_profil">
+        <h2>Welcome in your profile! </h2>
+    </div>
+    <h4>add your file here! </h4>
+    <form action="profil.php" method="POST" enctype="multipart/form-data">
+        <input type="file" name="files"> 
+        <input type="submit" name="sendFile" value="add"> <br> <br>
+    </form>
+    <table class="table_profile">
+        <tr>
+            <td>File name</td>
+            <td>Rename</td>
+            <td>Delete</td>
+            <td>Download</td>
+        </tr>
+        <tr>
+            <td><?= $success ?> </td>
+            <td> </td>
+            <td> </td>
+            <td> </td>
+        </tr>
+    </table>
+</article>
 
 <?php
 $content = ob_get_contents();
